baad97cba4a86909270efc504c3be7e5c46e04f7
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-01 00:59:09 -0500

    validateUserId middleware complete

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,7 +1,6 @@
 const User = require('../users/users-model');
 
 function logger(req, res, next) {
-  // DO YOUR MAGIC
   const reqMethod = req.method;
   const reqUrl = req.originalUrl;
   const timestamp = new Date().toLocaleString();
@@ -10,8 +9,19 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  const { id } = req.params;
+  const user = await User.getById(id);
+  try {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ message: 'user not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'error validating user id' });
+  }
 }
 
 function validateUser(req, res, next) {
@@ -22,4 +32,9 @@ function validatePost(req, res, next) {
   // DO YOUR MAGIC
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
