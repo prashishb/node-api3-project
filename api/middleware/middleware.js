c199ba7a903daf8b012ed06865c2926830ee408e
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-01 00:51:07 -0500

    logger middleware complete

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,13 @@
+const User = require('../users/users-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const reqMethod = req.method;
+  const reqUrl = req.originalUrl;
+  const timestamp = new Date().toLocaleString();
+
+  console.log(`${reqMethod} ${reqUrl} ${timestamp}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
